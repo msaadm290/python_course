85f6bbcf0bc7b4aabb0a3b4d313805673779ec70
saad <muhammadsmunaf@gmail.com>
2023-08-30 22:51:13 -0500

    update new clas list

---
+++ class_list.py
@@ -0,0 +1,47 @@
+#Methode how to use the list and its types
+
+#mylist = ["apple" , "banana" , "cherry"]
+
+#Lists are used to store multiple items in a single variable.
+
+#Lists are created using square brackets:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#List Items
+#List items are ordered, changeable, and 
+# allow duplicate values.
+
+#List items are indexed, the first item has index [0], 
+# the second item has index [1] etc.
+
+#Ordered
+#When we say that lists are ordered, it means that the items have a defined order, and that order will not change.
+
+#If you add new items to a list, the new items will 
+#be placed at the end of the list.
+
+#Lists allow duplicate values:
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+#List Length
+#Print the number of items in the list:
+
+thislist = ["apple", "banana", "cherry", "mango"]
+print(len(thislist))
+
+#List Items - Data Types
+#String, int and boolean data types:
+
+list1 = ["apple", 'banana', 'cherry']
+list2 = [10, 20, 30, 40, 50, 8]
+list3 = [False, True, False]
+
+print(list1)
+print(list2)
+print(list3)
+
+
