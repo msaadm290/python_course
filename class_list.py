734be6c4aae759d9f9aaa4f080437d87525a8376
saad <muhammadsmunaf@gmail.com>
2023-08-31 23:29:45 -0500

    update class_list

--- class_list.py
+++ class_list.py
@@ -72,7 +72,62 @@ print(thislist)
 thislist = ['apple', 'banana', 'cherry']
 print(thislist[2])
 
+#Negative Indexing
+#-1 refers to the last item, 
+# -2 refers to the second last item etc.
 
+#Print the last item of the list:
+
+thislist = ['apple', 'banana', 'cherry']
+print(thislist[-2])
+
+#Range of Indexes
+
+#You can specify a range of indexes by 
+#specifying where to start and where to end the range.
+
+#When specifying a range, the return value will be
+# a new list with the specified items.
+
+#Return the third, fourth, and fifth item:
+
+thislist = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
+print(thislist[2:5])
+
+#By leaving out the start value, 
+# the range will start at the first item:
+
+thislist = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
+print(thislist[:6])
+
+#By leaving out the end value, the range 
+# will go on to the end of the list:
+
+#This example returns the items 
+# from "cherry" to the end:
+
+thislist = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
+print(thislist[2:])
+
+#Range of Negative Indexes
+
+#Specify negative indexes if you want to start 
+# the search from the end of the list:
+
+#This example returns the items from "orange" 
+#(-4) \ to, but NOT including "mango" (-1):
+
+thislist = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
+print(thislist[-4:-1])
+
+#Check if Item Exists
+#To determine if a specified item is 
+#present in a list use the in keyword:
+
+thislist = ['apple', 'banana', 'cherry']
+if 'apple' in thislist:
+   print("yes, 'apple' is in the fruits list")
+   
 
 
 
