4f64c1a5d4d487301c8a3756c0f635f7f26c5b33
saad <muhammadsmunaf@gmail.com>
2023-09-05 00:02:18 -0500

    update class_list and class_loop

--- class_list.py
+++ class_list.py
@@ -317,6 +317,48 @@ thislist = ['monday', 'thuesday', 'wednesday']
 for i in range(len(thislist)):
    print(thislist[i])
 
+   #Using a While Loop
+
+   #You can loop through the 
+   # list items by using a while loop.
+
+   #Use the len() function to determine the length of the list, then start at 0 and loop your way through the list 
+   #items by referring to their indexes.
+
+#Remember to increase the index by 1 after each iteration.
+
+#Print all items, using a while loop to 
+#go through all the index numbers ????
+
+thislist = ['apple', 'banana', 'cherry']
+i - 0 
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+  #Looping Using List Comprehension
+
+  #List Comprehension offers the 
+  #shortest syntax for looping through lists:
+
+#A short hand for loop that 
+#will print all items in a list:  ???
+
+thislist = ['monday', 'tuesday', 'wednesday']
+[print(x) for x in thislist]
+
+#List Comprehension
+
+#List comprehension offers a shorter syntax 
+# when you want to create a new list based on 
+# the values of an existing list.
+
+#Example:
+
+#Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name.
+
+#Without list comprehension you will have to write a for statement with a conditional test inside:
+
 
 
 
---
+++ class_loop.py
@@ -0,0 +1,45 @@
+employees = ["saad","raheel","insha","Ali","zaid"]
+
+employeeName = "Raheel wali"
+
+# for emp in employees:
+#     print("Employee Name is:",emp)
+
+
+# for e in employeeName:
+#     print("Employee Name is:",e)
+
+
+# Break
+
+for emp in employees:
+    if emp == "saad":
+        break
+    print("Employee Name is: break",emp)
+
+
+for emp in employees:
+    print("Employee Name is: break",emp)
+    if emp == "saad":
+        break
+    
+
+# continue
+
+
+for emp in employees:
+    if emp == "raheel":
+        continue
+    print("Continue:",emp)
+
+
+# for emp in employees:
+#     print("Continue:",emp)
+#     if emp == "raheel":
+#         continue
+    
+
+
+
+ 
+
